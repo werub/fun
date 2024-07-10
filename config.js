735022db6699ca4b5fa3603502be87a475dbbfbd
huynamboz <teahuynam2003@gmail.com>
2024-07-10 11:41:44 +0700

    init

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+  introTitle: 'Bae à!',
+  introDesc: `Em có biết người ta hay gọi anh là gì không`,
+  btnIntro: '???',
+  title: 'Gọi anh là điệp viên 007',
+  desc: '0 lừa dối',
+  desc: '0 lòng vòng',
+  btnYes: 'Thế 7 gì ??',
+  btnNo: '0 có cơ hội đâu',
+  question: '7 GIỜ TỐI NAY VÔ MEET VỚI ANH NHA',
+  btnReply: 'Gửi em link đi ^^',
+  reply: 'EM ĐỒNG Ý',
+  mess: 'Oke nuônnnn',
+  messDesc: 'Hẹn cô nương vào tối nay nhé <3',
+  btnAccept: 'Hehee',
 }
